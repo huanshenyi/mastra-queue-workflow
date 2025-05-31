fab03aa9c49279c67f3a0c07f69b6e11b875eb15
huanshenyi <txy1226052@gmail.com>
2025-05-31 23:14:23 +0900

    chore: add stack tag

--- bin/iac.ts
+++ bin/iac.ts
@@ -1,6 +1,10 @@
-#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { IacStack } from '../lib/iac-stack';
 
 const app = new cdk.App();
-new IacStack(app, 'IacStack', {});
+new IacStack(app, 'IacStack', {
+    tags: {
+        Application: 'Mastra',
+        Environment: 'Production',
+    }
+});
