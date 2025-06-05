f378ff0aa0c868447f20690a7804f62b915a357a
huanshenyi <txy1226052@gmail.com>
2025-06-06 00:44:34 +0900

    chore: test

--- mastra/src/lib/bedrock-provider.ts
+++ mastra/src/lib/bedrock-provider.ts
@@ -3,17 +3,17 @@ import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
 
 export function initializeBedrockClient() {
   const region = process.env.REGION || "us-east-1";
-  if (process.env.NODE_ENV === "production") {
-    return createAmazonBedrock({
-      region: region,
-      credentialProvider: fromNodeProviderChain(),
-    });
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   return createAmazonBedrock({
+  //     region: region,
+  //     credentialProvider: fromNodeProviderChain(),
+  //   });
+  // }
 
   return createAmazonBedrock({
     region: region,
     accessKeyId: process.env.ACCESS_KEY_ID,
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    sessionToken: "",
+    sessionToken: "test",
   });
 }
